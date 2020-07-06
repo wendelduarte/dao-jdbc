df1f82307284c8bca1b98ea910c68e7097037d68
Wendel Duarte <wendel_duarte@outlook.com>
2020-07-06 15:02:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
+		Seller sell = new Seller(21, "Wendel", "wendel@hotmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(sell);
 		
 		System.out.println(obj);
 
